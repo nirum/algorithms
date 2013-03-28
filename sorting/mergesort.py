1178b7dd41736962c27fa24fd2e992367302a4a0
Niru Maheswaranathan <nirum@stanford.edu>
2013-03-27 20:02:30 -0700

    fixed mergesort, run it for a few different sizes

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -1,19 +1,15 @@
 import numpy as np
+import time as t
 
 # Merge operation
-def merge(A,p,q,r):
+def merge2(L,R):
 
-    # length of sub-arrays
-    n1 = q - p + 1;
-    n2 = r - q;
+    A = np.zeros(len(L)+len(R))                         # empty joint array
+    L = np.append(L,np.inf)                             # tack on inf to left array
+    R = np.append(R,np.inf)                             # tack on inf to right array
 
-    # build the sub-arrays (tack an Inf on the end)
-    L = A[p:r];   L[n1+1] = np.inf;                     # left  array
-    R = A[r+1:q]; R[n2+1] = np.inf;                     # right array
-
-    # run through the sub-arrays
-    i = j = 0;
-    for k in range(p,r+1):
+    i = j = 0
+    for k in np.arange(0,len(A)):
         if L[i] <= R[j]:                                # left comes first
             A[k] = L[i]                                 # put 1st left element into array
             i+=1
@@ -27,19 +23,28 @@ def merge(A,p,q,r):
 def mergesort(A,p,r):
     if p < r:
         q = np.floor((p+r)/2)                           # find a midpoint
-        mergesort(A,p,q)                                # sort the left branch
-        mergesort(A,q+1,r)                              # sort the right branch
-        merge(A,p,q,r)                                  # merge the results
+        Al = mergesort(A,p,q)                           # sort the left branch
+        Ar = mergesort(A,q+1,r)                         # sort the right branch
+        A = merge2(Al,Ar)                               # merge the results
+    else:
+        A = A[p]
 
     return A
 
 # run mergesort
 if __name__=="__main__":
 
-    # generate a random array
-    A = np.random.randint(0,100,(20,1))
-    print "Unsorted:\n" + repr(A) + "\n"
+    # run over different problem sizes
+    maxSize = 15
+    sizes = 2**np.arange(0,maxSize+1)
 
-    # run mergesort
-    A = mergesort(A,1,len(A))
-    print "Sorted:\n" + repr(A) + "\n"
+    # for loop over sizes
+    for s in sizes:
+
+        # generate a random array
+        A = np.random.randint(0,2**maxSize,(s,1))
+
+        # run mergesort
+        start = t.time()
+        A = mergesort(A,0,len(A)-1)
+        print "size: " + repr(s) + ")\t Elapsed time:\t" + repr(t.time() - start)
